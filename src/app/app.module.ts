fb0881bebd86b3f9f1a1ba688c86499dee413e5f
divekarvinit <divekarvinit@gmail.com>
2019-02-14 00:23:04 +1300

    Added providers

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import { AppComponent } from './app.component';
 import { FirstCarouselComponent } from './first-carousel/first-carousel.component';
 import { SecondCarouselComponent } from './second-carousel/second-carousel.component';
 import { CarouselService }from './services/carousel.service';
+import { GlobalConstant } from './global-constant';
 
 @NgModule({
   declarations: [
@@ -18,9 +20,10 @@ import { CarouselService }from './services/carousel.service';
     BrowserModule,
     FormsModule,
     AppRoutingModule,
-    NgbModule
+    NgbModule,
+    HttpClientModule
   ],
-  providers: [CarouselService],
+  providers: [CarouselService, GlobalConstant],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
